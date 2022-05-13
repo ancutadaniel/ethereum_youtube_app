bcc34966ef70b41b3bdbc89259462895911af22d
Daniel Ancuta <danielancuta@dacether.local>
2022-05-13 10:36:34 +0300

    deploy heroku

--- truffle-config.js
+++ truffle-config.js
@@ -18,12 +18,12 @@
  *
  */
 
-require('dotenv').config();
+require('dotenv').config({ path: './.env' });
 
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 
-const fs = require('fs');
-const mnemonic = fs.readFileSync('.secret').toString().trim();
+// const fs = require('fs');
+// const mnemonic = fs.readFileSync('.secret').toString().trim();
 
 module.exports = {
   /**
@@ -65,14 +65,23 @@ module.exports = {
     ropsten: {
       provider: () =>
         new HDWalletProvider(
-          mnemonic,
-          `https://ropsten.infura.io/v3/YOUR-PROJECT-ID${process.env.INFURA_ROPSTEN_ID}`
+          process.env.MNEMONIC,
+          `wss://ropsten.infura.io/ws/v3/${process.env.INFURA_ROPSTEN_ID}`
         ),
       network_id: 3, // Ropsten's id
       gas: 5500000, // Ropsten has a lower block limit than mainnet
-      // confirmations: 2, // # of confs to wait between deployments. (default: 0)
-      // timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
-      // skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+      confirmations: 2, // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+    },
+    rinkeby: {
+      provider: () =>
+        new HDWalletProvider(
+          process.env.MNEMONIC,
+          `https://rinkeby.infura.io/v3/${process.env.INFURA_RINKEBY_ID}`
+        ),
+      network_id: 4, // Rinkeby's id
+      gas: 5500000, // Rinkeby has a lower block limit than mainnet
     },
     // Useful for private networks
     // private: {
